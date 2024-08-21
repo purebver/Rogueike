9b14e0a5831218656c70776027f1bd40912784d7
purebver <wkddj1541@gmail.com>
2024-08-22 08:59:18 +0900

    2024.08.21

--- game.js
+++ game.js
@@ -1,35 +1,55 @@
 import chalk from 'chalk';
 import readlineSync from 'readline-sync';
+import fs from 'fs';
+import {achieve} from './server.js';
+
+export const delay = (delaytime) => new Promise((a) => setTimeout(a, delaytime - 1000))
 
 class Player {
-  constructor() {
-    this.hp = 100;
+  constructor(hp, power) {
+    this.hp = hp;
+    this.power = power;
+    this.twice = 30;
+    this.defense = 30;
   }
 
-  attack() {
+  attack(target) {
     // 플레이어의 공격
+    let ap = Math.floor(this.power * (Math.random() * 0.21 + 0.9))
+    target.hp -= ap;
+    achieve.attack++;
+    return ap;
+  }
+  recovery() {
+    this.hp += this.power
   }
 }
 
 class Monster {
-  constructor() {
-    this.hp = 100;
+  constructor(mul) {
+    this.hp = Math.floor(20 * mul * (Math.random() * 0.22 + 0.8));
+    this.power = Math.floor(5 * mul * (Math.random() * 0.22 + 0.8));
   }
 
-  attack() {
+  attack(target) {
     // 몬스터의 공격
+    let ap = Math.floor(this.power * (Math.random() * 0.21 + 0.9))
+    target.hp -= ap;
+    return ap;
   }
 }
+
+
 
 function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
     chalk.blueBright(
-      `| 플레이어 정보`,
+      `| 플레이어 정보`, '체력:' + player.hp, '공격력:' + player.power,
     ) +
     chalk.redBright(
-      `| 몬스터 정보 |`,
+      `| 몬스터 정보 |`, '체력:' + monster.hp, '공격력:' + monster.power,
     ),
   );
   console.log(chalk.magentaBright(`=====================\n`));
@@ -37,37 +57,197 @@ function displayStatus(stage, player, monster) {
 
 const battle = async (stage, player, monster) => {
   let logs = [];
-
-  while(player.hp > 0) {
+  while (player.hp > 0) {
     console.clear();
     displayStatus(stage, player, monster);
 
-    logs.forEach((log) => console.log(log));
-
     console.log(
       chalk.green(
-        `\n1. 공격한다 2. 아무것도 하지않는다.`,
+        `\n1. 공격한다 2. 연속공격 한다(${player.twice}%)  3. 방어하며 회복한다(${player.defense}%) 4. 도망간다`,
       ),
     );
     const choice = readlineSync.question('당신의 선택은? ');
 
     // 플레이어의 선택에 따라 다음 행동 처리
-    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+    console.log(chalk.green(`${choice}를 선택하셨습니다.`));
+    await delay(1000);
+
+    switch (choice) {
+      case '1':
+        console.log(chalk.blueBright(`공격`));
+        await delay(1000);
+        console.log(chalk.blueBright(`몬스터는${player.attack(monster)} 데미지를 받았다`));
+        await delay(1000);
+        if (monster.hp <= 0) {
+          break;
+        }
+        console.log(chalk.redBright(`몬스터의 공격`));
+        await delay(1000);
+        console.log(chalk.redBright(`당신은${monster.attack(player)} 데미지를 받았다`));
+        await delay(1000);
+        break;
+      case '2':
+        if (Math.random() < player.twice / 100) {
+          console.log(chalk.blueBright('연속공격!'));
+          await delay(1000);
+          console.log(chalk.blueBright(`몬스터는${player.attack(monster)} 데미지를 받았다`));
+          console.log(chalk.blueBright(`몬스터는${player.attack(monster)} 데미지를 받았다`));
+          await delay(1000);
+
+          if (monster.hp <= 0) {
+            break;
+          }
+
+          console.log(chalk.redBright(`몬스터의 공격`));
+          await delay(1000);
+          console.log(chalk.redBright(`당신은${monster.attack(player)} 데미지를 받았다`));
+          await delay(1000);
+        } else {
+          console.log(chalk.blueBright('당신은 연속공격에 실패했다'));
+          await delay(1000);
+          console.log(chalk.redBright(`몬스터의 공격`));
+          await delay(1000);
+          console.log(chalk.redBright(`당신은${monster.attack(player)} 데미지를 받았다`));
+          await delay(1000);
+        }
+        break;
+      case '3':
+        if (Math.random() < player.defense / 100) {
+          console.log(chalk.blueBright('당신은 몬스터의 공격을 성공적으로 막으면 회복을 취했다!'));
+          await delay(1000);
+          console.log(chalk.blueBright(`체력+${player.power}`));
+          player.recovery();
+          await delay(1000);
+        } else {
+          console.log(chalk.blueBright('당신은 방어에 실패했다'));
+          await delay(1000);
+          console.log(chalk.redBright(`몬스터의 공격`));
+          await delay(1000);
+          console.log(chalk.redBright(`당신은${monster.attack(player)} 데미지를 받았다`));
+          await delay(1000);
+        }
+        break;
+      case '4':
+        if (stage === 1) {
+          console.log('더이상 뒤가 없다.');
+          await delay(1000);
+        } else {
+          console.log('도망중 공격을 받았다.');
+          await delay(1000);
+          monster.attack(player);
+          await delay(1000);
+          return false; //도주 확인용
+        }
+        break;
+      default:
+        console.log(chalk.red('올바른 선택을 해주세요.'));
+        await delay(1000);
+    }
+    if (player.hp <= 0) {
+      console.log(chalk.redBright('당신은 죽었습니다.'));
+      achieve.death++;
+      fs.writeFileSync('achieve.json', JSON.stringify(achieve, null, 2), 'utf8');
+      console.log('5초뒤 게임이 종료 됩니다');
+      for (let i = 5; i > 0; i--) {
+        console.log(i);
+        await delay(1000);
+      }
+      process.exit(0);
+    }
+    if (monster.hp > 0) {
+      continue;
+    } else {
+      break;
+    }
   }
-  
+  return true; //도주한게 아님을 나타냄
 };
 
-export async function startGame() {
+
+export async function startGame(hp, power) {
+  achieve.start++;
   console.clear();
-  const player = new Player();
+  const player = new Player(hp, power);
+
   let stage = 1;
 
   while (stage <= 10) {
     const monster = new Monster(stage);
-    await battle(stage, player, monster);
+    const temp = await battle(stage, player, monster); //배틀 및 도주 확인용 리턴값
+
+    let reward = 0;
+
+    // 스테이지에서 도주
+    if (!temp) {
+      if (player.hp <= 0) {
+        //도주중 맞고 죽을경우
+        console.log(chalk.redBright('당신은 죽었습니다.'));
+        achieve.death++;
+        fs.writeFileSync('achieve.json', JSON.stringify(achieve, null, 2), 'utf8');
+        console.log('5초뒤 게임이 종료 됩니다');
+        for (let i = 5; i > 0; i--) {
+          console.log(i);
+          await delay(1000);
+        }
+        process.exit(0);
+      }
+      stage--;
+      continue;
+    }
 
     // 스테이지 클리어 및 게임 종료 조건
 
+    if (monster.hp > 0) {
+      continue;
+    } else {
+      console.log('당신은 몬스터를 쓰러뜨렸다.');
+      await delay(1000);
+      console.log(
+        chalk.green(
+          `\n1. 체력을 올린다 2. 공격력을 올린다 3. 연속공격 확률을 올린다 4. 방어률을 올린다`,
+        ),
+      );
+      const choice = readlineSync.question('당신의 선택은? ');
+      console.log(chalk.green(`${choice}를 선택하셨습니다.`));
+      await delay(1000);
+      switch (choice) {
+        case '1':
+          reward = Math.floor(stage * 12 * (Math.random() + 0.5));
+          console.log(chalk.green(`체력이 ${reward} 올랐다`));
+          player.hp += reward;
+          break;
+        case '2':
+          reward = Math.floor(stage * 4 * (Math.random() + 0.5));
+          console.log(chalk.green(`공격력이 ${reward} 올랐다`));
+          player.power += reward;
+          break;
+        case '3':
+          reward = Math.round(stage * (Math.random() + 2));
+          console.log(chalk.green(`연속공격 확률이 ${reward}% 올랐다`));
+          player.twice += reward;
+          break;
+        case '4':
+          reward = Math.round(stage * (Math.random() + 2));
+          console.log(chalk.green(`방어률이 ${reward}% 올랐다`));
+          player.defense += reward;
+          break;
+        default:
+          console.log(chalk.red('선택하지 않았습니다'));
+      }
+      await delay(1000);
+    }
     stage++;
   }
+
+  //클리어 했을 경우
+  console.log(chalk.blue('축하합니다 게임을 클리어 하셨습니다.'));
+  achieve.clear++;
+  fs.writeFileSync('achieve.json', JSON.stringify(achieve), 'utf8');
+  await delay(1000);
+  console.log('5초뒤 게임이 종료 됩니다');
+  for (let i = 5; i > 0; i--) {
+    console.log(i);
+    await delay(1000);
+  }
+  process.exit(0);
 }
--- server.js
+++ server.js
@@ -1,7 +1,61 @@
 import chalk from 'chalk';
 import figlet from 'figlet';
 import readlineSync from 'readline-sync';
-import {startGame} from "./game.js";
+import { startGame, delay } from "./game.js";
+import fs from 'fs';
+
+export let achieve;
+
+async function loadachieve() {
+    if (fs.existsSync('./achieve.json')) {
+        const data = await fs.promises.readFile('./achieve.json', 'utf8');
+        achieve = JSON.parse(data);
+    } else {
+        console.log('achieve.json 파일이 없어 새로 생성합니다.');
+        achieve = {
+            start: 0,
+            death: 0,
+            attack: 0,
+            clear: 0,
+        };
+        fs.writeFileSync('achieve.json', JSON.stringify(achieve), 'utf8');
+    }
+}
+
+
+//플레이어 기본 능력치
+let hp = 100;
+let power = 10;
+
+//업적 달성 시 추가 능력치
+const bonus = [0, 0];
+async function achievebonus() {
+    await loadachieve();
+
+    if (achieve.start > 0) {
+        bonus[0] += 10;
+    }
+    if (achieve.death > 4) {
+        bonus[0] += 40;
+    }
+    if (achieve.death > 9) {
+        bonus[0] += 50;
+    }
+    if (achieve.attack > 9) {
+        bonus[1] += 2;
+    }
+    if (achieve.attack > 19) {
+        bonus[1] += 3;
+    }
+    if (achieve.attack > 49) {
+        bonus[1] += 5;
+    }
+    if (achieve.clear > 0) {
+        bonus[0] += 100;
+        bonus[1] += 10;
+    }
+}
+
 
 // 로비 화면을 출력하는 함수
 function displayLobby() {
@@ -43,23 +97,63 @@ function displayLobby() {
 }
 
 // 유저 입력을 받아 처리하는 함수
-function handleUserInput() {
+async function handleUserInput() {
     const choice = readlineSync.question('입력: ');
 
     switch (choice) {
         case '1':
             console.log(chalk.green('게임을 시작합니다.'));
+            hp += bonus[0];
+            power += bonus[1];
+            await delay(1000);
             // 여기에서 새로운 게임 시작 로직을 구현
-            startGame();
+            startGame(hp, power);
             break;
         case '2':
-            console.log(chalk.yellow('구현 준비중입니다.. 게임을 시작하세요'));
             // 업적 확인하기 로직을 구현
+            console.log(chalk.yellow('달성된 업적 목록 입니다.'));
+            achieve.start > 0 ? console.log('산뜻한 출발 - 업적 완료') : console.log('숨겨진 업적');
+            achieve.death > 4 ? console.log('좀비? - 업적 완료') : console.log('숨겨진 업적');
+            achieve.attack > 19 ? console.log('일단공격해! - 업적 완료') : console.log('숨겨진 업적');
+            achieve.clear > 0 ? console.log('클리어! WOW 축하해요 - 업적 완료') : console.log('숨겨진 업적');
+            const temp = readlineSync.question('아무글자나 눌러주세요:');
+            displayLobby();
             handleUserInput();
             break;
         case '3':
-            console.log(chalk.blue('구현 준비중입니다.. 게임을 시작하세요'));
             // 옵션 메뉴 로직을 구현
+            console.log(
+                chalk.blue(
+                    `\n1. 초기 체력 수치를 더한다 2. 초기 공격력을 더한다 3. 초기선택화면으로`,
+                ),
+            );
+            const choice = readlineSync.question('원하시는 옵션을 선택해 주세요 ');
+            switch (choice) {
+                case '1':
+                    console.log(chalk.blue('초기 체력 수치 조정 100 ~ 200'));
+                    const hpupdown = Number(readlineSync.question('원하시는 수치를 입력해 주세요.'));
+                    if (hpupdown < 100 && hpupdown > 200) {
+                        console.log(chalk.blue('값이 이상합니다'));
+                        await delay(1000);
+                        break;
+                    }
+                    hp = hpupdown;
+                    break;
+                case '2':
+                    console.log(chalk.blue('초기 공격력 수치 조정 10 ~ 20'));
+                    const poupdown = Number(readlineSync.question('원하시는 수치를 입력해 주세요:'));
+                    if (poupdown < 10 && poupdown > 20) {
+                        console.log(chalk.blue('값이 이상합니다'));
+                        await delay(1000);
+                        break;
+                    }
+                    power = poupdown;
+                    break;
+                default:
+                    displayLobby();
+                    handleUserInput();
+            }
+            displayLobby();
             handleUserInput();
             break;
         case '4':
@@ -74,8 +168,9 @@ function handleUserInput() {
 }
 
 // 게임 시작 함수
-function start() {
+async function start() {
     displayLobby();
+    await achievebonus();
     handleUserInput();
 }
 
